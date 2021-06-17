b94cf3c07088e0df89b751fb4daf5c1796f5f435
shibafu <civaf-th123@hotmail.co.jp>
2021-06-17 13:22:36 +0900

    Node.js上で動作

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>おちんぽ</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +22,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
