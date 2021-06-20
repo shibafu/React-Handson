a22d408a7eb4a0d94a5c9d119702764aeba1da6e
shibafu <civaf-th123@hotmail.co.jp>
2021-06-20 16:14:50 +0900

    図形作図

---
+++ my-app/src/Rect.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class Rect extends React.Component {
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+    color = "White"
+    radius = 0
+    style = {}
+  constructor(props){
+    super(props)
+    this.x = props.x
+    this.y =  props.y 
+    this.width = props.width 
+    this.height = props.height
+    this.color = props.color
+    this.radius = props.radius
+    this.style = {
+        backgroundColor:this.color,
+        position:"absolute",
+        left:this.x,
+        top:this.y,
+        width:this.width + "px",
+        height:this.height + "px",
+        borderRadius:this.radius + "px",
+    }
+
+  }
+  render() {
+    return <div className="App">
+      <div style={this.style}>aaa</div>
+    </div>
+  }
+}
+
+export default Rect;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Rect from './Rect';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="Reactへようこそ！" message="ないしょのプログラムだよ"/>
+    {/* <App title="Reactへようこそ！" message="ないしょのプログラムだよ"/> */}
+    <Rect   x = "200"  y = "200"  width = "200"  height = "200"  color = "cyan"  radius = "100"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
