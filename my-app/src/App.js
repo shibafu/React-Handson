1483da899f4993e9ed45125f41e576a190a27814
shibafu <civaf-th123@hotmail.co.jp>
2021-06-23 17:23:27 +0900

    イベントリスナーを設定する

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,28 +3,44 @@ import './App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
+  // コンストラクタ
   constructor(props){
     super(props)
     this.state = {
       msg:"Hello Component",
       count:0,
+      button_msg:"Hello Component",
+      button_count:0
     };
     this.msg = props.msg
 
     let timer = setInterval(() => {
       this.setState({
         count: this.state.count + 1,
-        msg: "Count is Now [ " + this.state.count + " ] ",
+        msg: "Count is Now [ " + this.state.count + " ] "
     })
+
+    // イベントリスナーを紐づけ
+    this.doAction = this.doAction.bind(this)
   }, 1000);
   }
+  // ボタンイベント
+  doAction(event){
+    this.setState({
+      button_count:this.state.button_count + 1,
+      button_msg: "ButtonCount is Now [ " + this.state.button_count + " ] "
+    })
+  }
+
+  // レンダリングメソッド
   render() {
     return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="subtitle">{this.title}</p>
-        <p>{this.props.msg}</p>
         <p>{this.state.msg}</p>
+        <p>{this.state.button_msg}</p>
+        <button onClick={this.doAction}> ぼたん</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -36,6 +52,7 @@ class App extends Component {
         >
           Learn React
         </a>
+
       </header>
     </div>
   }
