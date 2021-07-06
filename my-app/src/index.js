0e3a229350bc5d4ac8451d8431d321a81ea73abd
shibafu <civaf-th123@hotmail.co.jp>
2021-07-07 08:52:28 +0900

    テーマコンテキストの設定

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="Reactへようこそ！" message="ないしょのプログラムだよ"/> 
+    <App /> 
     {/*<Rect   x = "200"  y = "200"  width = "200"  height = "200"  color = "#6ff9"  radius = "25"/> */}
     {/*<Rect   x = "300"  y = "300"  width = "200"  height = "200"  color = "#f6f9"  radius = "75"/> */}
     {/*<Rect   x = "400"  y = "400"  width = "200"  height = "200"  color = "#6669"  radius = "100"/> */}
