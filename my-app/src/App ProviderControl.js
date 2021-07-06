3f43525d2a2bc9603d81d4f16d12e7ea3475c4e5
shibafu <civaf-th123@hotmail.co.jp>
2021-07-07 08:19:43 +0900

    プロバイダー作成

---
+++ my-app/src/App ProviderControl.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+let data = {title:'React-Context',
+message:'this is sample message.'}
+
+const SampleContext = React.createContext(data)
+
+// アプリメインクラス
+class App extends Component {
+  // 描画メソッド
+  newdata = {title:'新しいタイトル',
+message:'新しいメッセージ'}
+  render(){
+    return <div>
+      <h1 className="bg-primary text-while display-4">React</h1>
+      <div className="container">
+        <Title />
+        <Message />
+        < hr />
+        <SampleContext.Provider value={this.newdata}>
+          <Title />
+          <Message />
+        </ SampleContext.Provider>
+        < hr />
+        <Title />
+        <Message />
+        < hr />
+      </div>
+    </div>
+
+  }
+}
+
+// タイトル描画メソッド
+class Title extends Component {
+  static contextType = SampleContext;
+
+  // 描画メソッド
+  render(){
+    return <div className="card p-2 my-3">
+      <h2>{this.context.title}</h2>
+    </div>
+  }
+}
+
+// メッセージ表示コンポーネント
+class Message extends Component {
+  static contextType = SampleContext;
+
+  // 描画メソッド
+  render(){
+    return <div className="alert alert-primary">
+      <p>{this.context.message}</p>
+    </div>
+  }
+}
+
+export default App;
