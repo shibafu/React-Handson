5bde5ca6ca7f20283af4c77c70a92b07c0bb1314
shibafu <civaf-th123@hotmail.co.jp>
2021-07-14 20:25:26 +0900

    コンポーネント内コンポーネント

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,5 +9,24 @@ import React, { Component, useState } from 'react';
     </div>)
 
   }
+  // テーマ
+  function CardMessage() {
+    return (<div className="card p-3 h5 border-primary text-center">
+      this is Alert Message
+    </div>)
 
-export default AlertMessage;
+  }
+  // テーマ
+  function App() {
+    return (<div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <div className="container">
+        <h4>Component Sample</h4>
+        <CardMessage />
+        <AlertMessage />
+      </div>
+    </div>)
+
+  }
+
+export default App;
