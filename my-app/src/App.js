ee2267e7fe592035f8967292aa97a12717a0cdc2
shibafu <civaf-th123@hotmail.co.jp>
2021-07-14 20:20:29 +0900

    関数コンポーネントのバリエーション

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,51 +3,11 @@ import './App.css';
 import React, { Component, useState } from 'react';
 
   // テーマ
-  function App(){
-    // カウントステートをセット
-    const [count, setCount] = useState(0)
-    const clickCountButtonFunc = () =>{
-      setCount(count + 1)
-    }
-    // フラグステートをセット
-    const [flag, setFlag] = useState(false)
-    const clickFlagButtonFunc = (e) =>{
-      if(flag){
-        setFlag(false)
-      }
-      else {
-        setFlag(true)
-      }
-    }
-
-    return (
-      <div>
-        <h1 className="bg-primary text-white display-4 ">React</h1>
-        <div className="container">
-          <h4 className="my-3">Hooks sample</h4>
-          {/* フラグステート */}
-          {flag ? 
-          <div className="alert alert-primary text-center ">
-            <p className="h5 mb-3">click: {count} times!</p>
-            <div><button className="btn btn-primary" onClick={clickCountButtonFunc} >クリック！</button></div>
-          </div>
-        :
-        <div className="card p-3 border-primary text-left ">
-          <p className="h5 mb-3">click: {count} times!</p>
-          <div><button className="btn btn-primary" onClick={clickCountButtonFunc} >クリック！</button></div>
-        </div>
-        }
-        {/* ボタンステート */}
-          <div className="alert alert-primary text-center">
-            <p className="h5 mb-3 text-left text-primary">Change Form Style!</p>
-            <div><button className="btn btn-primary" onClick={clickFlagButtonFunc} >クリック！</button></div>
-          </div>
-        </div>
-      </div>
-
-
-    )
+  function AlertMessage() {
+    return (<div className="alert alert-primary h5 text-primary">
+      this is Alert Message
+    </div>)
 
   }
 
-export default App;
+export default AlertMessage;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import AlertMessage from './App';
 import Rect from './Rect';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+  <AlertMessage /> 
+    {/*<App />*/} 
     {/*<Rect   x = "200"  y = "200"  width = "200"  height = "200"  color = "#6ff9"  radius = "25"/> */}
     {/*<Rect   x = "300"  y = "300"  width = "200"  height = "200"  color = "#f6f9"  radius = "75"/> */}
     {/*<Rect   x = "400"  y = "400"  width = "200"  height = "200"  color = "#6669"  radius = "100"/> */}
